42ac17651cb846653a1143899b3cbd47df7d0e24
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-06-18 16:00:24 +0530

    added Hero page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.js'
+import Hero from './Hero.js'
+import Navbar from './navbar.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Navbar />
+    <Hero />
   </StrictMode>,
 )
