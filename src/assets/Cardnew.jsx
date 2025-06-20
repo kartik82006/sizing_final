1a2f1aed1cc8127c63bdd6c25a7b6ebb802bc63f
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-06-20 10:34:58 +0530

    added changes to design

--- src/assets/Cardnew.jsx
+++ src/assets/Cardnew.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import image1 from "../images/smr.jpeg";
 import image2 from "../images/winged_plane.jpeg";
+import image3 from "../images/quad.jpg"
+import image4 from "../images/tiltrotor.jpeg"
 
-const images = [image1, image2, image1,image2];
+const images = [image1, image2, image3,image4];
 
 function Cardnew({ className = "" }) {
   return (
@@ -10,7 +12,7 @@ function Cardnew({ className = "" }) {
       <div className="h-[700px] p-1 flex flex-col items-center justify-start border-2 border-gray-300 overflow-y-scroll p-0 m-0 box-border gap-4">
         {images.map((image, index) => (
           <div key={index}
-            className="card-list w-[80%] aspect-[4/3] md:max-h-[200px] lg:max-h-[240px] flex-shrink-0 rounded-3xl flex relative p-0 m-0 box-border overflow-hidden"
+            className="card-list w-[80%] aspect-[4/3] md:max-h-[200px] lg:max-h-[240px] flex-shrink-0 rounded-3xl flex relative p-0 m-0 box-border overflow-visible"
           >
             <img
               src={image}
