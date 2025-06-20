f7f820034bff8b01d29319694869e3f9520b7f14
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-06-20 16:01:14 +0530

    added minor changes

--- src/assets/Hero.jsx
+++ src/assets/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
   const [showCard,setShowCard] = useState(false); 
 
   return (
-    <div className="flex flex-row justify-center bg-[#1995AD] items-center font-bold font-delius-regular overflow-y-scroll h-[80vh] box-border">
+    <div className="flex flex-row pb-[5%] mb-[3%] justify-center bg-[#1995AD] items-center font-bold font-delius-regular overflow-y-scroll h-[93vh] box-border">
       {/* Left Section: Slide in from left */}
       <motion.div
         initial={{ x: -200, y: 20, opacity: 0 }}
@@ -19,7 +19,7 @@ function Hero() {
         transition={{ duration: 1, ease: "easeOut" }}
         className="hero m-2 pt-6 border-4 border-black bg-[#B3C8CF] flex-[6] h-full overflow-y-scroll items-start text-center justify-center rounded-lg box-border"
       >
-        <h1 className="text-5xl text-[#1995AD] edu-nsw-act-hand-pre-700 font-bolder m-8 mt-12 justify-center flex">Let’s Get You Airborne</h1>
+        <h1 className="text-5xl text-[#1995AD] edu-nsw-act-hand-pre-700 font-bolder m-6 justify-center flex">Let’s Get You Airborne</h1>
 
         <h1 className="text-2xl font-bold j">Our intuitive tool lets you select your eVTOL configuration and input key design parameters to instantly generate sizing calculations, performance estimates, and design insights—streamlining your concept development process.
           </h1>
--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -3,7 +3,7 @@ import logo2 from "../images/logo.png"; // Ensure path is correct
 
 function Navbar() {
   return (
-    <nav className="fade-in-navbar fixed top-0 left-0 right-0 z-50 bg-[#E5E1DA] rounded-b-xl shadow-md px-4 sm:px-6 lg:px-10 mt-1 mx-2">
+    <nav className="fade-in-navbar fixed top-0 left-0 right-0 z-50 bg-[#E5E1DA] border-4 border-black rounded-b-xl shadow-md px-4 sm:px-6 lg:px-10 mt-1 mx-2">
       <div className="flex items-center h-20 sm:h-24 lg:h-28 max-w-[1440px] mx-auto rounded-lg">
         <img
           src={logo2}
