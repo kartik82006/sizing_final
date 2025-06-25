01b50e2af3090ba549d27d9e46414b977a144a54
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-06-25 10:25:09 +0530

    small changes

--- src/assets/main.jsx
+++ src/assets/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import Try from './try.jsx'
+// import Navigation from './Navigation.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <App/>
+   {/* <App/> */}
+  <App/>
   </StrictMode>,
 )
 
